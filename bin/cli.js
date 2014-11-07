9403090658a98af9e3ec49aa541055f3e001e94d
Douglas Duteil <douglasduteil@gmail.com>
2014-11-07 20:43:18 +0100

    SAVE

---
+++ bin/cli.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var nomnom = require("nomnom");
+var bemComment = require("../");
+var fs = require('vinyl-fs');
+var through2 = require('through2');
+var pkg = require('../package.json');
+
+var options = nomnom
+    .script('bem-comment')
+    .help(pkg.description)
+    .options({
+        path: {
+            position: 0,
+            help: 'BEM files to comment',
+            required: true,
+            list: true
+        },
+        version: {
+            flag: true,
+            help: 'Print version',
+            callback: function() {
+                return pkg.version;
+            }
+        }
+    })
+    .parse();
+
+fs.src(options.path || [])
+    .pipe(bemComment())
+    .pipe(through2.obj(function(file, enc, callback) {
+        console.log(file.contents.toString());
+        console.log(file.path);
+        callback(null, file);
+    }))
+    //.pipe(fs.dest('./'))
+    ;
--- index.js
+++ index.js
@@ -6,101 +6,83 @@
  * Licensed under the MIT license.
  */
 
-var LineByLineReader = require('line-by-line');
-var fs = require('fs');
-var cli = require('cli');
+'use strict';
 
-var commentBuff = {};
-var previousLine = '';
-var stdout = [];
+var through2 = require('through2');
 
-module.exports = {
-    processFiles: function() {
-        cli.args.forEach(function(src) {
-            processFile(src);
-        });
-    }
-};
+module.exports = function bemComment() {
 
-/**
- * Read a file and add full class name
- * @param {string} src
- */
-function processFile(src) {
-    commentBuff = {};
-    previousLine = '';
-    stdout = [];
+    var commentBuff;
 
-    var lr = new LineByLineReader(src);
+    return through2.obj(function transform(file, enc, callback) {
+        commentBuff = [];
 
-    lr.on('line', function(line) {
-        stdout.push(processLine(line));
-        previousLine = line;
+        var lines = file.contents.toString().split(/(?:\n|\r\n|\r)/g);
+        file.contents = Buffer(lines.map(processLine).join('\n'));
+
+        callback(null, file);
     });
 
-    lr.on('end', function() {
-        fs.writeFile(src, stdout.join('\n') + '\n');
-    });
 
-    lr.on('error', function(err) {
-        throw err;
-    });
-}
+    /**
+     * Process a line adding the comment if supported
+     * @param {string} line The current line
+     * @param {number} index The current line number
+     * @param {string[]} lines All the lines
+     * @returns {string} the processed line.
+     */
+    function processLine(line, index, lines) {
+        var isClassLine = /^\s*[\.&][^\s]+/gi.test(line);
 
-/**
- * Extract the needed info for class line
- * @param {string} line
- * @returns {string}
- */
-function processLine(line) {
-    var isClassLine = /^\s*[\.&][^\s]+/gi.test(line);
+        if (!isClassLine || isUnsupportedSelector(line, index, lines)) {
+            return line;
+        }
 
-    if (!isClassLine) {
+        var indentation = '';
+        var name = '';
+        var isReferencingParentSelectors= 0;
+        line.replace(/(\s*)(\.|&)([^\s{]+)/,
+            function(match, selectorIndentation, selectorStartingCharacter, selectorName) {
+                isReferencingParentSelectors |= selectorStartingCharacter === '&';
+                indentation = selectorIndentation;
+                name = selectorName;
+            }
+        );
+
+        if (isReferencingParentSelectors){
+            commentBuff.push(name);
+        }else{
+            commentBuff = [name];
+        }
+
+        // The last line is a comment
+        if (index > 0 && /^\s*\/\//.test(lines[index - 1])){
+          return line;
+        }
+
+        line = indentation + '\/\/ ' + commentBuff.join('') + '\n' + line;
+
+        commentBuff.splice(commentBuff.length - (line.match(/}/g) || []).length, 1);
+
         return line;
     }
 
-    checkPreviousLine();
+    /**
+     * Test is the selector in the line is unsupported
+     * @param {string} line The current line
+     * @param {number} index The current line number
+     * @param {string[]} lines All the lines
+     * @returns {number} 0 or 1 if the selector in the line is unsupported
+     */
+    function isUnsupportedSelector(line, index, lines) {
 
-    var spaces = /^\s+/gi.exec(line);
-    var level = spaces ? spaces[0].length : 0;
-    var name = line.trim();
+        var isUnsupported = 0;
 
-    // Sub classes need to have a different format
-    if (level === 0 || name.charAt(0) !== '.') {
-        name = name.replace(/[\.&\s{]/gi, '');
-    } else {
-        name = ' ' + name.replace(/[\.\s{]/gi, '');
+        // Is not a isolated class
+        isUnsupported |= !(/^\s*(?:\.|&)[^\s\.:\[]+\s*\{/.test(line));
+        // More than one definition... too hard; don't do
+        isUnsupported |= (line.match(/{/g) || []).length > 1;
+
+        return isUnsupported;
     }
-
-    commentBuff[level] = name;
-
-    return buildComment(level) + '\n' + line;
-}
-
-/**
- * Return the complete class name comment
- * @param {number} level
- * @returns {string}
- */
-function buildComment(level) {
-    var comment = '\/\/ ';
-
-    for (var i = 0; i <= level; i++) {
-        comment = ' ' + comment;
-
-        if (commentBuff[i]) {
-            comment += commentBuff[i];
-        }
-    }
-
-    return comment.substr(1);
-}
-
-/**
- * If the previous line is already a comment, we remove it
- */
-function checkPreviousLine() {
-    if (/^\s*[\/\/]/gi.test(previousLine)) {
-        stdout.splice(-1);
-    }
-}
+};
